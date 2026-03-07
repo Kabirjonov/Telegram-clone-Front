581fdeab84d493572be66cfddcf9c9020f8aaa9e
vibecodeing <vibecodeing@example.com>
2026-03-07 22:01:46 +0500

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono, Figtree } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+
+const figtree = Figtree({subsets:['latin'],variable:'--font-sans'});
 
 const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
@@ -29,7 +31,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en' suppressHydrationWarning className={cn("font-mono", jetbrainsMono.variable)}>
+		<html lang='en' suppressHydrationWarning className={cn( jetbrainsMono.variable, "font-sans", figtree.variable)}>
 			<body
 				suppressHydrationWarning
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
